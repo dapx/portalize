cae792f7df4df447e1bb9a611e85ab7d02d2b3ac
Daniel André da Silva <dap1995@gmail.com>
2020-08-11 23:08:50 -0300

    Change to use sh rather than bash to install

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 rcFile=".$(basename $SHELL)rc"
 cp ~/$rcFile ~/$rcFile.bkp.$(date +%y%m%d%H%M%S)
 
 portalize_function_alias="alias portalize='make_portal() { echo alias \$1=\$PWD >> ~/$rcFile; unset -f make_portal; source ~/$rcFile }; make_portal'"
-unportalize_function_alias="alias unportalize='destroy_portal() { printf \"%s\\n\" \"g/alias \$1=/d\" w | ed -s ~/$rcFile; unalias \$1; unset -f destroy_portal; source ~/$rcFile }; destroy_portal'"
+unportalize_function_alias="alias unportalize='destroy_portal() { printf \"%s\\\\n\" \"g/alias \$1=/d\" w | ed -s ~/$rcFile; unalias \$1; unset -f destroy_portal; source ~/$rcFile }; destroy_portal'"
 
 echo $portalize_function_alias >> ~/$rcFile
 echo $unportalize_function_alias >> ~/$rcFile
