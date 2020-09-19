7be4cf2ee8df245c5628c9ee27ef7550db29f12d
Daniel André da Silva <dap1995@gmail.com>
2020-09-19 10:04:23 -0300

    Change to save aliases in a separate file called portals

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 rcFile=".$(basename $SHELL)rc"
+portalsFile=".portals"
 cp ~/$rcFile ~/$rcFile.bkp.$(date +%y%m%d%H%M%S)
 
-portalize_function_alias="alias portalize='make_portal() { echo alias \$1=\$PWD >> ~/$rcFile; unset -f make_portal; source ~/$rcFile }; make_portal'"
-unportalize_function_alias="alias unportalize='destroy_portal() { printf \"%s\\\\n\" \"g/alias \$1=/d\" w | ed -s ~/$rcFile; unalias \$1; unset -f destroy_portal; source ~/$rcFile }; destroy_portal'"
+portalize_function_alias="alias portalize='make_portal() { echo alias \$1=\$PWD >> ~/$portalsFile; unset -f make_portal; source ~/$rcFile }; make_portal'"
+unportalize_function_alias="alias unportalize='destroy_portal() { printf \"%s\\\\n\" \"g/alias \$1=/d\" w | ed -s ~/$portalsFile; unalias \$1; unset -f destroy_portal; source ~/$rcFile }; destroy_portal'"
 
-echo $portalize_function_alias >> ~/$rcFile
-echo $unportalize_function_alias >> ~/$rcFile
+portalsFileLoad=". ~/$portalsFile"
 
-unset portalize_function_alias unportalize_function_alias rcFile
+echo $portalsFileLoad >> ~/$rcFile
+echo $portalize_function_alias >> ~/$portalsFile
+echo $unportalize_function_alias >> ~/$portalsFile
+
+unset portalize_function_alias unportalize_function_alias rcFile portalsFile portalsFileLoad
